fc564aea0441e5711e84ac938f3e85cce754b347
Nadine Coelho <nadinecoelho9@gmail.com>
2021-07-28 01:03:53 -0300

    finish creating states and update input search

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,56 @@ const App = () => {
   const apiKey = process.env.REACT_APP_API_KEY;
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  });
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${apiId}&app_key=${apiKey}`
+      `https://api.edamam.com/search?q=${query}&app_id=${apiId}&app_key=${apiKey}`
     );
     const data = await response.json();
     setRecipes(data.hits);
   };
 
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
   return (
     <div className="App">
-      <form className="seacr-form">
-        <input className="search-form__bar" type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-form__bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button className="search-form__button" type="submit">
           Search
         </button>
       </form>
 
-      {recipes.map((recipe) => (
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-        />
-      ))}
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 };
