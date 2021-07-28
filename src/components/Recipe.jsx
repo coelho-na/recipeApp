94f025c6f9b2b221c0a077232914a26b9ce503ce
Nadine Coelho <nadinecoelho9@gmail.com>
2021-07-28 01:02:45 -0300

    add components to show the recipes

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import styles from "../recipe.module.css";
 
-const Recipes = ({ title, calories, image }) => {
+const Recipes = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{calories}</p>
-      <img src={image} alt={title} />
+    <div className={styles.recipe}>
+      <h1 className={styles.recipe__title}>{title}</h1>
+      <ol className={styles.recipe__orderedList}>
+        {ingredients.map((ingredient) => (
+          <li className={styles.recipe__list}>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>Calorias: {Math.floor(calories.toFixed(1))}kcal</p>
+      <img className={styles.recipe__img} src={image} alt={title} />
     </div>
   );
 };
